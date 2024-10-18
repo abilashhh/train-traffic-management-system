f5246ddbe6968c62f8176a3af9f38ad2cc04d1af
Abilash Karthi <91648734+abilashhh@users.noreply.github.com>
2024-10-18 23:05:13 +0100

    Update firebase.js

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -5,13 +5,13 @@ import { getAuth } from 'firebase/auth';
 
 const firebaseConfig = {
 
-  apiKey: "AIzaSyCEAr3PpVZS3pZFgbHMW1noEkhbJwo39Iw",
-  authDomain: "test-project-ec1c2.firebaseapp.com",
-  projectId: "test-project-ec1c2",
-  storageBucket: "test-project-ec1c2.appspot.com",
-  messagingSenderId: "726339624415",
-  appId: "1:726339624415:web:72c33d1c85b8198e13f86a",
-  measurementId: "G-YR1ZL65D69"
+  apiKey: "Enter your api Key",
+  authDomain: " ",
+  projectId: " ",
+  storageBucket: " ",
+  messagingSenderId: " ",
+  appId: " ",
+  measurementId: " "
 };
 
 // Initialize Firebase
